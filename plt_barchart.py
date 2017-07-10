a66efd33813e32e108b9480b968ad3af319da230
utsavsolanki <utsav.solanki@live.com>
2017-07-10 12:55:15 +0530

    Barplot Example with matplot library

--- plt_barchart.py
+++ plt_barchart.py
@@ -4,9 +4,17 @@ import numpy as np
 # simple bar and scatter plot
 x = np.arange(5) # assume there are 5 students
 y = (20, 35, 30, 35, 27) # their test scores
+
+#bar chart
 plt.bar(x,y) # Bar plot
 # need to close the figure using show() or close(), if not closed any follow
 #up plot commands will use same figure.
 plt.show() # Try commenting this an run
+
+#Scatter Plot
 plt.scatter(x,y) # scatter plot
 plt.show()
+
+# Line Chart
+plt.plot(x,y)
+plt._show()
