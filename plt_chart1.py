6cdc0d73606138050b5a68d8b6e284c2b5d7b259
utsavsolanki <utsav.solanki@live.com>
2017-07-10 13:19:06 +0530

    Barplot Example with matplot library

--- plt_chart1.py
+++ plt_chart1.py
@@ -9,6 +9,10 @@ y = (20, 35, 30, 35, 27) # their test scores
 plt.bar(x,y) # Bar plot
 # need to close the figure using show() or close(), if not closed any follow
 #up plot commands will use same figure.
+plt.xlabel("X Label")
+plt.ylabel("Y Label")
+plt.title("Bar Chart")
+plt.savefig("Barchart")
 plt.show() # Try commenting this an run
 
 #Scatter Plot
