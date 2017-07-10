cfce20071ba77bc014ec68ee0a53e991638722cf
utsavsolanki <utsav.solanki@live.com>
2017-07-10 12:50:14 +0530

    Barplot Example with matplot library

--- plt_barchart.py
+++ plt_barchart.py
@@ -8,5 +8,5 @@ plt.bar(x,y) # Bar plot
 # need to close the figure using show() or close(), if not closed any follow
 #up plot commands will use same figure.
 plt.show() # Try commenting this an run
-#plt.scatter(x,y) # scatter plot
-#plt.show()
+plt.scatter(x,y) # scatter plot
+plt.show()
